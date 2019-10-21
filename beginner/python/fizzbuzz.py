d00bfac16a44ea4d8d3f18b7eeebfaecf9088018
Nnanyielugo <kz.onyegbule@gmail.com>
2019-10-21 09:36:01 +0100

    remove local var for adjusted index, modify start and stop indices instead

--- beginner/python/fizzbuzz.py
+++ beginner/python/fizzbuzz.py
@@ -1,12 +1,11 @@
 def fizzbuzz(num):
-  for _, ind in enumerate(range(num)):
-    adjusted_index = ind + 1
-    emit = adjusted_index
-    if adjusted_index % 3 == 0 and adjusted_index % 5 == 0:
+  for _, ind in enumerate(range(1, num + 1)):
+    emit = ind
+    if ind % 3 == 0 and ind % 5 == 0:
       emit = 'fizzbuzz'
-    elif adjusted_index % 3 == 0:
+    elif ind % 3 == 0:
       emit = 'fizz'
-    elif adjusted_index % 5 == 0:
+    elif ind % 5 == 0:
       emit = 'buzz'
     print(emit)
 
