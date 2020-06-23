4b9b44f01f2ccc4ca934d5c1977c3fff3692c776
Nnanyielugo <kz.onyegbule@gmail.com>
2020-06-23 08:56:41 +0100

    use multiple assignment on python

--- beginner/javascript/fibonacci.js
+++ beginner/javascript/fibonacci.js
@@ -1,29 +1,14 @@
 function fibonacci(n){
   let a = 0;
   let b = 1;
-  let temp;
 
-  for (let i = 2; i <= n; i++) {
-    temp = a + b;
+  for (let i = 0; i < n; i++) {
+    let temp = a + b;
     a = b;
     b = temp
-    console.log(b)
+    console.log(a)
   }
-  return b
+  return a
 }
 
 console.log(fibonacci(10))
-
-// function fibonacci(n){
-//   let a = 0;
-//   let b = 1;
-
-//   while (n >= 0){
-//     let temp = a + b;
-//     a = b;
-//     b = temp;
-//     n--;
-//     console.log(a)
-//   }
-//   return a
-// }
--- beginner/python/fibonacci.py
+++ beginner/python/fibonacci.py
@@ -2,12 +2,23 @@ def fib_loop(n):
   a = 0
   b = 1
 
-  while n >= 0:
+  while n > 0:
     temp = a + b
     a = b
     b = temp
-    n -= 1
     print(a)
+    n -= 1
   return a
 
-print(fib_loop(10))
+def fib_loop_multiple_assign(n):
+  a, b = 0, 1
+
+  while n > 0:
+    # use tuple packing and unpacking to simulteneously swap values 
+    # and avoid using a temp variable for 'a + b'
+    a, b = b, a + b
+    print(a)
+    n -= 1
+  return a
+
+print(fib_loop_multiple_assign(10))
