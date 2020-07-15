d0f8df7cd056c867555ce76805e7cc5c0b69d625
Nnanyielugo <kz.onyegbule@gmail.com>
2020-07-15 14:26:00 +0100

    falsy bouncer

---
+++ beginner/javascript/falsy-bouncer.js
@@ -0,0 +1,29 @@
+/**
+ * A falsy value is a value that is considered false when examined as a Boolean. 
+ * Recall that a Boolean value could either be true or false. 
+ * JavaScript uses type conversion to translate any value to true or false when required. 
+ * Examples of falsy values are: false, null, undefined, 0, NaN, and "".
+ * 
+ * Given an array, remove all falsy values from the array and return an array of only truthy values.
+ */
+
+function falsyBouncer(values) {
+  const outputValues = []
+  values.forEach(value => {
+    if (Boolean(value)) {
+      outputValues.push(value)
+    }
+  })
+  return outputValues
+}
+
+function falsyBouncerWReduce(values) {
+  return values.reduce((acc, curr) => {
+    if (curr) {
+      acc.push(curr)
+    }
+    return acc
+  }, [])
+}
+
+console.log(falsyBouncerWReduce([1, 0, null, '', 5]))
