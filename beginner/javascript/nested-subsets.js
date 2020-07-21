f9621b41089c9238cdd38453869b7538bd239d57
Nnanyielugo <kz.onyegbule@gmail.com>
2020-07-21 22:47:50 +0100

    change loop types

--- beginner/javascript/nested-subsets.js
+++ beginner/javascript/nested-subsets.js
@@ -2,10 +2,8 @@
 function checkNestedSubsets(list1, list2) {
   let tempList2 = [...list2]
   let count = 0;
-  for (let i = list1.length - 1; i >= 0; i--) { // loop in the reverse direction of the spicing
-    let list1Arr = list1[i]
-    for (let j = tempList2.length - 1; j >= 0; j--) {
-      let list2Arr = tempList2[j]
+  for (let list1Arr of list1) { // loop in the reverse direction of the spicing
+    for (let list2Arr of tempList2) {
       let bool = list2Arr.every(num => {
         let equalLengths = list1Arr.length === list2Arr.length;
         return equalLengths && list1Arr.includes(num)
