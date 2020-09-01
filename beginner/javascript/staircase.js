f9c55b15d467ec9e1b516db7554a70a2d330f98b
Nnanyielugo <kz.onyegbule@gmail.com>
2020-09-01 15:54:03 +0100

    staircase recurive

--- beginner/javascript/staircase.js
+++ beginner/javascript/staircase.js
@@ -12,4 +12,25 @@ function staircase(n, reverse=false) {
   }
 }
 
+
+function staircase(n, row=0, stair='') {
+  // exit clause
+  if (row === n) return;
+
+  // row clause - increment row - fresh stair
+  if (stair.length === n) {
+    console.log(stair);
+    return staircase(n, row+1);
+  }
+
+  // stair clause
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  return staircase(n, row, stair);
+}
+
 staircase(9)
