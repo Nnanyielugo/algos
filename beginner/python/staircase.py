a8507fa4d2f00ed678ff7e1993cffd5a3918fe5e
Nnanyielugo <kz.onyegbule@gmail.com>
2020-09-06 02:42:03 +0100

    reverse function from iteirative solutions

--- beginner/python/staircase.py
+++ beginner/python/staircase.py
@@ -1,29 +1,37 @@
-# def staircase(n, reverse=False):
-#   for i in range(n):
-#     output = ''
-#     for j in range(n, 0, -1):
-#       if i < (j - 1):
-#         if reverse:
-#           output += ' '
-#         else:
-#           output += ''
-#       else:
-#         output += '#'
-#     print(output)
-
-def staircase(n, row = 0, stair = ''):
-  if n == row:
-    return
-
-  if len(stair) == n:
+def staircase(n):
+  for row in range(n):
+    stair = ''
+    for col in range(n):
+      if (row >= col):
+        stair += '#'
+      else:
+        stair += ' '
     print(stair)
-    return staircase(n, row + 1)
 
-  if len(stair) <= row:
-    stair += '#'
-  else:
-    stair += ' '
+def staircaseRev(n):
+  for row in range(n):
+    stair = ''
+    for col in range(n-1, -1, -1):
+      if row >= col:
+        stair += '#'
+      else:
+        stair += ' '
+    print(stair)
+
+# staircase recursive function
+# def staircase(n, row = 0, stair = ''):
+#   if n == row:
+#     return
+
+#   if len(stair) == n:
+#     print(stair)
+#     return staircase(n, row + 1)
+
+#   if len(stair) <= row:
+#     stair += '#'
+#   else:
+#     stair += ' '
   
-  return staircase(n, row, stair)
+#   return staircase(n, row, stair)
 
-staircase(9)
+staircaseRev(9)
