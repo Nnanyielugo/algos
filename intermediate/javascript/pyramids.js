5c1f3cea0b5db866b2c76a5c25925279572ce4f1
Nnanyielugo <kz.onyegbule@gmail.com>
2020-09-07 16:37:47 +0100

    move column count and midpoint vars out of the loops

--- intermediate/javascript/pyramids.js
+++ intermediate/javascript/pyramids.js
@@ -1,9 +1,9 @@
 function pyramids(n) {
+  const colCount = 2 * n - 1;
+  const midpoint = Math.floor(colCount / 2);
   for (let row = 0; row < n; row++) {
     let level = '';
-    let colCount = 2 * n - 1;
     for (let col = 0; col < colCount; col++) {
-      let midpoint = Math.floor(colCount / 2);
       let range = (midpoint + row >= col) &&  (midpoint - row <= col);
       if (range) {
         level += '#';
--- intermediate/python/pyramids.py
+++ intermediate/python/pyramids.py
@@ -1,11 +1,11 @@
 from math import floor
 
 def pyramid(n):
+  col_count = n * 2 - 1
+  midpoint = floor(col_count /2)
   for row in range(n):
     level = ''
-    col_count = n * 2 - 1
     for col in range(col_count):
-      midpoint = floor(col_count /2)
       pyramid_range = (midpoint + row >= col) and (midpoint - row <= col)
       if pyramid_range:
         level += '#'
