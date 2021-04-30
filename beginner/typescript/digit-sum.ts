556790629854d9f8db47967717ffcac816bcd297
Nnanyielugo <kz.onyegbule@gmail.com>
2021-04-30 16:12:09 +0100

    digit-sum

---
+++ beginner/typescript/digit-sum.ts
@@ -0,0 +1,14 @@
+/**
+  Write a function called digit_sum that takes a positive integer n as input and returns the sum of all that number’s digits. 
+  For example: digit_sum(1234) should return 10 which is 1 + 2 + 3 + 4. 
+  (Assume that the number you are given will always be positive.)
+ */
+
+function digit_sum(n: number): number {
+  const stringified_num = n.toString();
+  let total = 0;
+  for (let char of stringified_num) {
+    total += parseInt(char);
+  }
+  return total;
+}
