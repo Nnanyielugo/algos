f4b3e956c6e3305dbf98ba7bec60472e7ded2b4b
Nnanyielugo <kz.onyegbule@gmail.com>
2021-06-04 12:34:58 +0100

    max length sublist - ts

---
+++ beginner/max-length-sublist/max-length-sublist.ts
@@ -0,0 +1,14 @@
+// Given a list of lists, write a program to find the list with maximum length. The output should be in the form (list, list_length).
+function maxLengthSublist(list: any[]): any[] {
+  const maxSubList = list.reduce((acc, curr) => {
+    if (acc.length > curr.length) {
+      return acc;
+    }
+    return curr;
+  }, []);
+
+  return [maxSubList, maxSubList.length];
+}
+
+console.log(maxLengthSublist([['A'], ['A', 'B'], ['A', 'B', 'C']]));
+console.log(maxLengthSublist([[1, 2, 3, 9, 4], [5], [3, 8], [2]]));
