416c3a253b344ea7586480849e0b754ba5925813
Nnanyielugo <kz.onyegbule@gmail.com>
2021-06-04 12:44:27 +0100

    longest word - ts

--- beginner/longest-word/longest-word.ts
+++ beginner/longest-word/longest-word.ts
@@ -2,8 +2,8 @@
  * Given a sentence, return the longest word in the string
  */
 
-function longestWord(sentence) {
-  return sentence.split(' ').reduce((acc, curr) => {
+function longestWord(sentence: string) {
+  return sentence.split(' ').reduce((acc: string, curr: string) => {
     if (curr.length > acc.length) {
       return curr;
     }
